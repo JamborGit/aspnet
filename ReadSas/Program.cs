a2c65bb4edfe9bda653ba45d0489c68f43be462f
jambor6 <support-v-jayao@hotmail.com>
2014-08-26 13:58:56 +0800

    sas

---
+++ ReadSas/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadSas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sas = "https://jamborstorage.blob.core.windows.net/sascontainer?sv=2014-02-14&sr=c&sig=E19ShpOtv4pv9VyROf%2BBxXNGVpcbCJl5VBBByAD6d14%3D&se=2014-08-26T09%3A54%3A27Z&sp=wl";
+            CloudBlobContainer container = new CloudBlobContainer(new Uri(sas));
+
+            //Create a list to store blob URIs returned by a listing operation on the container.
+            List<Uri> blobUris = new List<Uri>();
+
+            try
+            {
+                //Write operation: write a new blob to the container. 
+                CloudBlockBlob blob = container.GetBlockBlobReference("blobCreatedViaSAS.txt");
+                string blobContent = "This blob was created with a shared access signature granting write permissions to the container. ";
+                MemoryStream msWrite = new MemoryStream(Encoding.UTF8.GetBytes(blobContent));
+                msWrite.Position = 0;
+                using (msWrite)
+                {
+                    blob.UploadFromStream(msWrite);
+                }
+                Console.WriteLine("Write operation succeeded for SAS " + sas);
+                Console.WriteLine();
+            }
+            catch (StorageException e)
+            {
+                Console.WriteLine("Write operation failed for SAS " + sas);
+                Console.WriteLine("Additional error information: " + e.Message);
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
